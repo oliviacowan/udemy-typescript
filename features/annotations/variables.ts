d37afde42baca6c997e5cf6707b1bd895546401a
Olivia <olivia.cowan@hotmail.com>
2022-04-13 18:28:13 +0000

    annotations with objects and destructuring

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -11,7 +11,7 @@ let now: Date = new Date();
 
 // Array
 let colors: string[] = ['red', 'green', 'blue'];
-let myNubers: number[] = [1,2,3];
+let myNubers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
 
 // Classes
@@ -21,10 +21,38 @@ let car: Car = new Car();
 // object literal
 let point: { x: number; y: number } = {
   x: 10,
-  y:20
-}
+  y: 20,
+};
 
 // Function
-const logNumber: (i: number) => void  = (i: number) => {
+const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations --
+
+// 1) function that returns 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) when we declare  variable on ne line and initailize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) when a variable whose type cannot be inferred correctly
+// (when using an 'or' in the type)
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
